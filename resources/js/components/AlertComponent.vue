86c41c44479595d7e3c6c39f76368f2190692fe4
Alfon5000 <alfonlai97@gmail.com>
2023-06-17 23:24:49 +0700

    Create HeaderComponent.vue, SidebarComponent.vue, AlertComponent.vue & clean Laravel & Vue.js code

---
+++ resources/js/components/AlertComponent.vue
@@ -0,0 +1,102 @@
+<script>
+export default {
+    name: "AlertComponent",
+    methods: {
+        updateTime() {
+            const dateElement = document.getElementById("date");
+            const timeElement = document.getElementById("time");
+
+            const currentDate = new Date();
+
+            const date = currentDate.toLocaleDateString(undefined, {
+                weekday: "short",
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+            });
+
+            const time = currentDate.toLocaleTimeString();
+
+            dateElement.innerText = date;
+            timeElement.innerText = time;
+        },
+    },
+    mounted() {
+        this.updateTime();
+        setInterval(this.updateTime, 1000);
+    },
+};
+</script>
+
+<template>
+    <div class="container-fluid">
+        <div class="container-fluid my-3">
+            <div id="datetime-widget">
+                <div id="time"></div>
+                <div id="date"></div>
+            </div>
+        </div>
+        <hr class="bg-light" />
+        <div class="container-fluid my-3">
+            <h5 class="text-center mb-3">Alerts</h5>
+            <div class="card" style="height: 250px">
+                <div class="card-header bg-danger text-center">Important</div>
+                <div class="card-body text-danger overflow-auto">
+                    <p>Important 1</p>
+                    <hr class="bg-danger" />
+                    <p>Important 2</p>
+                    <hr class="bg-danger" />
+                    <p>Important 3</p>
+                    <hr class="bg-danger" />
+                    <p>Important 4</p>
+                    <hr class="bg-danger" />
+                    <p>Important 5</p>
+                </div>
+            </div>
+            <div class="card" style="height: 250px">
+                <div class="card-header bg-warning text-center">Warning</div>
+                <div class="card-body text-warning overflow-auto">
+                    <p>Warning 1</p>
+                    <hr class="bg-warning" />
+                    <p>Warning 2</p>
+                    <hr class="bg-warning" />
+                    <p>Warning 3</p>
+                    <hr class="bg-warning" />
+                    <p>Warning 4</p>
+                    <hr class="bg-warning" />
+                    <p>Warning 5</p>
+                </div>
+            </div>
+            <div class="card" style="height: 250px">
+                <div class="card-header bg-info text-center">Informational</div>
+                <div class="card-body text-info overflow-auto">
+                    <p>Informational 1</p>
+                    <hr class="bg-info" />
+                    <p>Informational 2</p>
+                    <hr class="bg-info" />
+                    <p>Informational 3</p>
+                    <hr class="bg-info" />
+                    <p>Informational 4</p>
+                    <hr class="bg-info" />
+                    <p>Informational 5</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#datetime-widget {
+    width: 100%;
+    text-align: center;
+}
+
+#time {
+    font-size: 20px;
+    font-weight: bold;
+}
+
+#date {
+    font-size: 14px;
+}
+</style>
---
+++ resources/js/components/SidebarComponent.vue
@@ -0,0 +1,83 @@
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+    name: "SidebarComponent",
+};
+</script>
+
+<template>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-3 bg-navy">
+        <!-- Brand Logo -->
+        <router-link :to="{ name: 'dashboard' }" class="brand-link">
+            <img
+                src="dist/img/AdminLTELogo.png"
+                alt="AdminLTE Logo"
+                class="brand-image img-circle elevation-3"
+                style="opacity: 0.8"
+            />
+            <span class="brand-text font-weight-light">DCIM</span>
+        </router-link>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul
+                    class="nav nav-pills nav-sidebar flex-column"
+                    data-widget="treeview"
+                    role="menu"
+                >
+                    <li class="nav-item">
+                        <router-link
+                            :to="{ name: 'dashboard' }"
+                            class="nav-link"
+                        >
+                            <i class="nav-icon fas fa-tachometer-alt mr-2"></i>
+                            <p>Dashboard</p>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link
+                            :to="{ name: 'visitors.index' }"
+                            class="nav-link"
+                        >
+                            <i class="nav-icon fas fa-users mr-2"></i>
+                            <p>Visitors</p>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link
+                            :to="{ name: 'users.index' }"
+                            class="nav-link"
+                        >
+                            <i class="nav-icon fas fa-user mr-2"></i>
+                            <p>Users</p>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'reports' }" class="nav-link">
+                            <i class="nav-icon fas fa-chart-bar mr-2"></i>
+                            <p>Reports</p>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'cctv' }" class="nav-link">
+                            <i class="nav-icon fas fa-video mr-2"></i>
+                            <p>CCTV</p>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'about' }" class="nav-link">
+                            <i class="nav-icon fas fa-info-circle mr-2"></i>
+                            <p>About</p>
+                        </router-link>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+</template>
