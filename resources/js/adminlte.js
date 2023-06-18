82012ac684f55d56d05060b02e952152160578f1
Alfon5000 <alfonlai97@gmail.com>
2023-06-18 11:13:14 +0700

    Import all dependencies for Admin LTE

--- resources/js/adminlte.js
+++ resources/js/adminlte.js
@@ -2,5 +2,27 @@
 import "admin-lte/dist/css/adminlte.min.css";
 import "admin-lte/dist/js/adminlte.min.js";
 
-// import Admin LTE plugins
+// import Admin LTE css plugins
 import "admin-lte/plugins/fontawesome-free/css/all.min.css";
+import "admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css";
+import "admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css";
+import "admin-lte/plugins/jqvmap/jqvmap.min.css";
+import "admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css";
+import "admin-lte/plugins/daterangepicker/daterangepicker.css";
+import "admin-lte/plugins/summernote/summernote-bs4.min.css";
+
+// import Admin LTE js plugins
+import "admin-lte/plugins/jquery/jquery.min.js";
+import "admin-lte/plugins/jquery-ui/jquery-ui.min.js";
+// $.widget.bridge("uibutton", $.ui.button);
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js";
+import "admin-lte/plugins/chart.js/Chart.min.js";
+import "admin-lte/plugins/sparklines/sparkline.js";
+import "admin-lte/plugins/jqvmap/jquery.vmap.min.js";
+import "admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js";
+import "admin-lte/plugins/jquery-knob/jquery.knob.min.js";
+import "admin-lte/plugins/moment/moment.min.js";
+import "admin-lte/plugins/daterangepicker/daterangepicker.js";
+import "admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js";
+import "admin-lte/plugins/summernote/summernote-bs4.min.js";
+import "admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js";
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ env('APP_NAME') }}</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
