737902b27e1876409952bf07a4089b4de46d14d1
Alfon5000 <alfonlai97@gmail.com>
2023-07-09 10:11:03 +0700

    Create CCTVPlayer component for CCTV page

---
+++ resources/js/components/CCTV/CCTVPlayer.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: "CCTVPlayer",
+    props: ["elementID", "streamUrl"],
+    data() {
+        return {
+            elementID: this.elementID,
+            streamURL: this.streamUrl,
+        };
+    },
+    methods: {
+        playCCTV() {
+            const cctvElement = document.getElementById(this.elementID);
+            const streamUrl = this.streamURL;
+            cctvElement.src = streamUrl;
+            cctvElement.onerror = () =>
+                console.log("Error occurred while loading the stream.");
+        },
+    },
+    mounted() {
+        this.playCCTV();
+    },
+};
+</script>
+
+<template>
+    <video :id="elementID"></video>
+</template>
--- resources/js/pages/CCTV.vue
+++ resources/js/pages/CCTV.vue
@@ -1,19 +1,10 @@
 <script>
-import { onMounted } from "vue";
+import CCTVPlayer from "../components/CCTV/CCTVPlayer.vue";
 
 export default {
     name: "CCTV",
-    methods: {
-        playCCTV() {
-            const cctvElement = document.getElementById("cctvElement");
-            const streamUrl = "rtsp://host:port/stream-name";
-            cctvElement.src = streamUrl;
-            cctvElement.onerror = () =>
-                console.log("Error occurred while loading the stream.");
-        },
-    },
-    mounted() {
-        // this.playCCTV();
+    components: {
+        CCTVPlayer,
     },
 };
 </script>
@@ -27,15 +18,38 @@ export default {
                 <h2>CCTV</h2>
             </div>
             <div class="content">
-                <iframe
-                    width="720"
-                    height="405"
-                    src="https://www.youtube.com/embed/ClMX6TXvh_o?autoplay=1&mute=1"
-                    frameborder="1"
-                    allowfullscreen
-                >
-                </iframe>
-                <!-- <video id="cctvElement" autoplay></video> -->
+                <div class="row">
+                    <div class="col-12 col-sm-6">
+                        <h4>Luar 1</h4>
+                        <CCTVPlayer
+                            elementID="luar_1"
+                            streamURL="rtsp://192.168.11.5"
+                        />
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <h4>Luar 2</h4>
+                        <CCTVPlayer
+                            elementID="luar_2"
+                            streamURL="rtsp://192.168.11.6"
+                        />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-6">
+                        <h4>Access Door</h4>
+                        <CCTVPlayer
+                            elementID="access_door"
+                            streamURL="rtsp://192.168.11.8"
+                        />
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <h4>Dalam</h4>
+                        <CCTVPlayer
+                            elementID="dalam"
+                            streamURL="rtsp://192.168.11.9"
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
